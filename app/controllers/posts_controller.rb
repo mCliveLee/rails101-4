9e40ac16b56185f93a0c5a9001c85e91a0190cc7
Clive <starlwj@gmail.com>
2017-05-16 23:16:26 +0800

    implement edit/delete for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,14 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
-  before_action :find_group, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, :only => [:edit, :update, :destroy]
 
   def new
+    @group = Group.find(params[:group_id])
     @post = Post.new
   end
 
   def create
+    @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
     @post.group = @group
     @post.user = current_user
@@ -18,13 +20,35 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post Deleted"
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
 
-  def find_group
-    @group = Group.find(params[:group_id])
+  def find_group_and_check_permission
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to root_path, alert: "You are not permission!"
+    end
   end
 end
